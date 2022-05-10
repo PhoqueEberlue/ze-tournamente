332aec8f869ed221343ba9ba0b0b9907eaa66f00
Baki <90310933+JoaquimHenriquesD@users.noreply.github.com>
2022-05-10 08:13:32 +0200

    Add files via upload

---
+++ Site/Mainpage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Mainpage.css"/>
+    <title>Ze Tournamente</title>
+</head>
+<body>
+
+<div id="Title">
+<h1 id="bigtitle">Ze Tournamente</h1>
+</div>
+<div id="contentlmao">
+    <div id="main">
+        <div id="menu">
+            <div><a href="" class="btn_menu">Ajouter Player</a></div>
+            <div><a href="" class="btn_menu">Ajouter Equipe</a></div>
+            <div><a href="" class="btn_menuacceuil" ><img src="https://icon-library.com/images/home-logo-icon/home-logo-icon-0.jpg"></a></div>  
+            <div><a href="" class="btn_menu ">Associer Player Equipe</a></div>   
+            <div><a href="" class="btn_menu">Ze Tournamente</a></div> 
+         
+        </div>
+    </div>
+</div>
+</body>
+</html>
