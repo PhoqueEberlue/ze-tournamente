731632494a695dad3e4cf7a8eb76728b3c20a3c3
Thomas Nicolas <77977775+KaiDen26@users.noreply.github.com>
2022-05-28 01:42:39 +0200

    Ajout affichage arbre tournoi

--- src/php/tournament.php
+++ src/php/tournament.php
@@ -1,3 +1,112 @@
+<?php
+    include "bdd.php";
+
+    $bdd = new BDD("localhost", "root", "", "zetournamente");
+
+    // liste des ID
+    /*$liste_equipes = array(
+        $bdd->add_equipe("les Branquignols"),
+        $bdd->add_equipe("ZCorp"),
+        $bdd->add_equipe("AnnéciensPasContents"),
+        $bdd->add_equipe("Les noobs"),
+        $bdd->add_equipe("Lacrimatica"),
+        $bdd->add_equipe("Pantoufle warrior"),
+        $bdd->add_equipe("Barochettes"),
+        $bdd->add_equipe("Les chocolatines"),
+        $bdd->add_equipe("test1"),
+        $bdd->add_equipe("test2")
+    );
+    $tournament_id = $bdd->add_tournoi("test_tournoi");
+    $bdd->set_liste_equipe($liste_equipes);
+    $bdd->create_tree(10, $tournament_id);  */
+
+    //select distinct max(round_match) from `match` where id_tournoi = 1
+
+
+
+    // En fonction du choix du form
+    $tournament_id = 1;
+    $round = $bdd->getFirstRoundId($tournament_id);
+    $matchs = $bdd->get_matchs($round, $tournament_id);
+
+    $tournament_format = array(2,4,8,16,32,64,128,264);
+
+    $teams = array();
+
+    foreach ($matchs as $match) {
+
+        $sub_teams = array();
+
+        $equipe1 = $bdd->get_equipe($match[1])[1];
+        $equipe2 = $bdd->get_equipe($match[2])[1];
+
+        $sub_teams[] = $equipe1;
+        $sub_teams[] = $equipe2;
+
+        $teams[] = $sub_teams;
+      
+    }
+
+    $teams_number = $bdd->get_equipes_number($tournament_id);
+
+    $value = (getClosest($teams_number, $tournament_format) - $teams_number) / 2;
+
+    for ($i = 0; $i < $value; $i++){
+        $teams[] = [null, null];
+    }
+
+    $results = [];
+    
+    
+    for ($i = $round; $i > 0; $i--){
+
+        $current_round = $i;
+
+        $matchs = $bdd->get_matchs($current_round, $tournament_id);
+
+        $round_scores = array();
+
+        foreach ($matchs as $match) {
+
+            $matchs_scores = array();
+            $score1 = (int) $match[3];
+            $score2 = (int) $match[4];
+
+            if(isset($score1)){
+                $matchs_scores[] = $score1;
+            }
+            if(isset($score2)){
+                $matchs_scores[] = $score2;
+            }
+
+            if(!empty($matchs_scores)){
+                $round_scores[] = $matchs_scores;
+            }
+           
+        }
+        $results[] = $round_scores;
+
+    }
+
+    function getClosest($search, $arr) {
+        $closest = null;
+        foreach ($arr as $item) {
+           if($search <= $item){
+                if ($closest === null || abs($search - $closest) > abs($item - $search)) {
+                    $closest = $item;
+                }
+           }
+
+         
+        }
+        return $closest;
+     }
+
+
+    
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +114,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/Mainpage.css"/>
+    <link rel="stylesheet" href="jquery.bracket.min.css"/>
     <title>Ze Tournamente</title>
 </head>
 <body class="body">
@@ -14,48 +124,36 @@
 </div>
 <div id="contentlmao">
     <div id="main">
-    <div id="menu">
-            <div id="box"><a href="form_player.php" class="btn_menu">Création joueur</a></div>
-            <div id="box"><a href="form_equipe.php" class="btn_menu">Création Equipe</a></div>
-            <div id="box"><a href="Mainpage.php" class="btn_menuacceuil" ><img src="https://icon-library.com/images/home-logo-icon/home-logo-icon-0.jpg"></a></div>  
-            <div id="box"><a href="form_add_team.php" class="btn_menu ">Création Tournois</a></div>   
-            <div id="box"><a href="tournament.php" class="btn_menu">Ze Tournamente</a></div> 
+        <div id="menu">
+                <div id="box"><a href="form_player.php" class="btn_menu">Création joueur</a></div>
+                <div id="box"><a href="form_equipe.php" class="btn_menu">Création Equipe</a></div>
+                <div id="box"><a href="Mainpage.php" class="btn_menuacceuil" ><img src="https://icon-library.com/images/home-logo-icon/home-logo-icon-0.jpg"></a></div>  
+                <div id="box"><a href="form_add_team.php" class="btn_menu ">Création Tournois</a></div>   
+                <div id="box"><a href="tournament.php" class="btn_menu">Ze Tournamente</a></div> 
         </div>
 
+        <div class="tournament"></div>
+
+        <script src="jquery.min.js"></script>
+        <script src="jquery.bracket.min.js"></script>
+        <script>
+
+
+        var minData = {
+
+            teams : <?php echo json_encode($teams); ?>
+            ,
+            results : <?php echo json_encode($results); ?>
         
+        }
+
+        $('.tournament').bracket({
+            init: minData
+        });
+
+        </script>    
     </div>
 </div>
 
-
-<form action="form.php" method="post">
-        <form-panel id ="panel">
-            <form-header id="Formheader">
-                <h3>Ajout d'un tournoi :</h3>
-            </form-header>
-
-            <form-content>
-                <form-group class="group">
-                    <label for="nomTournoi">Nom :</label>
-                    <input type="text" name="nomTournoi" id="nomTournoi" />
-                </form-group>
-            
-                <form-group class="group">
-                    <label for="regle">Regle :</label>
-                    <input type="text" name="regle" id="regle" />
-                </form-group>
-
-                <form-group class="group">
-                    <input type="submit" name="btn4" value="Ajouter" />
-                </form-group>
-            </form-content>
-        </form-panel>
-
-    </form>
 </body>
 </html>
-
-<?php
-echo"test";
-
-
-?>
